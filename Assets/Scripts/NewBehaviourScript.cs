9737b5237dd9f3735ac363c9ee1933362ee02cc1
FranciscoAbreuFreire58667 <fg.freire@campus.fct.unl.pt>
2023-05-10 23:59:32 +0100

    King

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject enemy;
-     public float distanceBetweenObjects = 2f;
+     public float distanceBetweenObjects = 4f;
      List<Vector3> points = new List<Vector3>();
       
     // Start is called before the first frame update
@@ -16,12 +16,12 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     void ObjectPositioning(){
-         int count = Random.Range(2, 6);
+         int count = Random.Range(50, 60);
  
          for(int i = 0; i < count;) {
-             float x = Random.Range(-2.0f, 2.0f);
-             float y = Random.Range(-4.0f, 4.0f);
-             Vector3 point = new Vector3(x, y, 0);
+             float x = Random.Range(-16.0f, 16.0f);
+             float z = Random.Range(-10.0f, 10.0f);
+             Vector3 point = new Vector3(x, 0, z);
 
              if (points.Count==0){
                  points.Add(point);
@@ -45,13 +45,18 @@ public class NewBehaviourScript : MonoBehaviour
  
      void ObjectSpawning(){
          for (int i = 0; i < points.Count; i++){
-             Instantiate(enemy, points[i], Quaternion.identity);
+            Instantiate(enemy, points[i], Quaternion.Euler(new Vector3(0,Random.Range(-180f, 180.0f),0)));
          }
      }
 
-    // Update is called once per frame
-    void Update()
+    /*public GameObject cubePrefab;
+
+    void Start()
     {
-        
-    }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
+            Instantiate(cubePrefab, randomSpawnPosition, Quaternion.identity);
+        }
+    }*/
 }
