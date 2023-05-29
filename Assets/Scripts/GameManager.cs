5119b12bf8f219b3de65d916cd9bd026b789f72d
FranciscoAbreuFreire58667 <fg.freire@campus.fct.unl.pt>
2023-05-29 01:01:35 +0100

    building placement completed?
    Co-Authored-By: PedroSimões_58674 <91724921+pedrosimoes28@users.noreply.github.com>

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,8 @@ public class GameManager : MonoBehaviour
     public GameObject inProgressHouse;
     public GameObject newHouse;
     public GameObject barrack;
+    public GameObject inProgressBarrack;
+    public GameObject newBarrack;
     public GameObject soldiers;
     public GameObject civilians;
     public LayerMask layersToHit;
@@ -26,20 +28,25 @@ public class GameManager : MonoBehaviour
     GameObject button4;
     GameObject button5;
     GameObject button6;
+    GameObject button7;
+    GameObject button8;
 
     GameObject mainCamera;
     GameObject secondCamera;
     GameObject selectedBarrack;
     GameObject selectedHouse;
     GameObject newHouseTransparent;
+    GameObject newBarrackTransparent;
 
     List<GameObject> selectedUnits;
     
 
     bool cheatCamera= false;
     bool placingBuilding = false;
+    bool placingBuildingBarrack = false;
     bool alreadyActivatedBarrack = false;
     bool alreadyActivatedHouse = false;
+    bool buildingActivateButtons= false;
 
     void Start()
     {
@@ -63,6 +70,12 @@ public class GameManager : MonoBehaviour
         button6 = GameObject.Find("Button (5)");
         button6.GetComponent<Button>().onClick.AddListener(ClickCivilian);
         button6.SetActive(false);
+        button7 = GameObject.Find("Button (6)");
+        button7.GetComponent<Button>().onClick.AddListener(ClickHouse);
+        button7.SetActive(false);
+        button8 = GameObject.Find("Button (7)");
+        button8.GetComponent<Button>().onClick.AddListener(ClickBarrack);
+        button8.SetActive(false);
 
         mainCamera = GameObject.Find("Main Camera");
         secondCamera = GameObject.Find("Camera");
@@ -92,6 +105,21 @@ public class GameManager : MonoBehaviour
             Instantiate(soldiers, new Vector3(child.transform.position.x - 1.38f, child.transform.position.y, child.transform.position.z - 1.38f), Quaternion.identity);
         }
     }
+    
+
+    public void ClickHouse()
+    {
+        placingBuilding = true;
+        newHouseTransparent = Instantiate(house,new Vector3(0,0,0), Quaternion.identity);
+        newHouseTransparent.tag = "TranspHouse";
+    }
+
+    public void ClickBarrack()
+    {
+        placingBuildingBarrack= true;
+        newBarrackTransparent = Instantiate(barrack,new Vector3(0,0,0), Quaternion.identity);
+        newBarrackTransparent.tag = "TranspBarrack";
+    }
 
     public void ClickTrain()
     {
@@ -105,7 +133,6 @@ public class GameManager : MonoBehaviour
             Instantiate(soldiers, new Vector3(child.transform.position.x, child.transform.position.y, child.transform.position.z), Quaternion.identity);
             Instantiate(soldiers, new Vector3(child.transform.position.x+1.38f, child.transform.position.y, child.transform.position.z+1.38f), Quaternion.identity);
             Instantiate(soldiers, new Vector3(child.transform.position.x - 1.38f, child.transform.position.y, child.transform.position.z - 1.38f), Quaternion.identity);
-
         }
     }
     private IEnumerator passiveMe(int secs, int buttonNum)
@@ -131,11 +158,18 @@ public class GameManager : MonoBehaviour
 
     public void ClickBuilding()
     {
-        
         Debug.Log("The building was clicked.");
-        placingBuilding = true;
-        newHouseTransparent = Instantiate(house,new Vector3(0,0,0), Quaternion.identity);
-        newHouseTransparent.tag = "TranspHouse";
+        if(!buildingActivateButtons){
+            button7.SetActive(true);
+            button8.SetActive(true);
+            buildingActivateButtons = true;
+        }else{
+            button7.SetActive(false);
+            button8.SetActive(false);
+            buildingActivateButtons = false;
+        }
+        
+        
     }
 
     public void ClickDefend()
@@ -243,6 +277,44 @@ public class GameManager : MonoBehaviour
                 Instantiate(inProgressHouse,hit.point, Quaternion.identity);
                 Destroy(newHouseTransparent);
                 placingBuilding = false;
+                button6.SetActive(false);
+            }
+            conflict = false;
+        }
+        }
+
+        if(placingBuildingBarrack){
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        LayerMask mask = LayerMask.GetMask("Environment");
+        if (Physics.Raycast(ray,  out  hit, 100000f, mask)) {
+            newBarrackTransparent.transform.position = new Vector3(hit.point.x, -1.66f, hit.point.z);
+        }
+        
+        if (Input.GetMouseButtonDown(0))
+        {                                  
+            Collider[] colliderNeighbors = Physics.OverlapSphere(newBarrackTransparent.transform.position, 10);
+            bool conflict = false;
+            //int LayerIgnoreRaycast = LayerMask.NameToLayer("Environment");
+            foreach (Collider collider in colliderNeighbors)
+            {
+                if (collider.gameObject.tag != "Map" && collider.gameObject.tag != "TranspBarrack")
+                {
+                    conflict= true;
+                        Debug.Log("--------------------------------------------------awful");
+                      Debug.Log(collider.gameObject.tag);
+                    break;
+                }
+            }
+            if(conflict){
+                  Debug.Log("--------------------------------------------------awful");
+            }
+            if(!conflict){
+                Debug.Log("--------------------------------------------------alright");
+                Vector3 vec = new Vector3 (hit.point.x, -1.66f, hit.point.z);
+                Instantiate(inProgressBarrack,vec, Quaternion.identity);
+                Destroy(newBarrackTransparent);
+                placingBuildingBarrack = false;
                 button6.SetActive(false);
             }
             conflict = false;
