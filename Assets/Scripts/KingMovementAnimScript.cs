f00ac4e8297bb2f7c2c934e239adbaad05e96c53
FranciscoAbreuFreire58667 <fg.freire@campus.fct.unl.pt>
2023-06-03 14:15:02 +0100

    king walk anim

--- Assets/Scripts/KingMovementAnimScript.cs
+++ Assets/Scripts/KingMovementAnimScript.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KingMovementScript : MonoBehaviour
+public class KingMovementAnimScript : MonoBehaviour
 {
     float speed;
     private MouseMove moveScript;
     private Animator animator;
+    private UnityEngine.AI.NavMeshAgent agent;
    
    void Awake(){
         moveScript = GetComponent<MouseMove>();
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
    }
 
     void Update() {
@@ -18,25 +20,19 @@ public class KingMovementScript : MonoBehaviour
         GameObject flag = GameObject.Find("kingFlag");
         if(flag != null){
             Vector3 vec = new Vector3(flag.transform.position.x,0,flag.transform.position.z);
-           // animator.SetBool("walking", true);
+            Vector3 vec2 = new Vector3(transform.position.x,0,transform.position.z);
+             if(Vector3.Distance(vec, vec2) < 1){
+                animator.SetBool("walking", false);
+                moveScript.Stop();
+            }else{
+            animator.SetBool("walking", true);
             moveScript.SetDestination(vec);
             moveScript.MoveToPos();
-           /* 
-            
-            if(Vector3.Distance (vec, transform.position) > 2){
-
-                speed = 3;
-                float step = speed * Time.deltaTime;
-                transform.LookAt(vec); 
-                transform.position = Vector3.MoveTowards(transform.position, vec, step);
-                
-            }else{
-                speed=0;
             }
-            */
         }else{
-            //animator.SetBool("walking", false);
+            animator.SetBool("walking", false);
             moveScript.Stop();
         }
     }
+    
 }
