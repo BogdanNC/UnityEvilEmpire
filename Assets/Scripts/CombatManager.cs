dc0900c3531e6423ddfd57cc02eaf856131f680b
Blackadder008 <d.mira@campus.fct.unl.pt>
2023-06-09 17:06:42 +0100

    Major bug fixes and GUI improvements

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class CombatManager : MonoBehaviour
 {
     [SerializeField] private int hp;
+    public int maxHP;
+
+    public GameObject hpBar;
 
     private void Awake()
     {
         //Eventually this will depend on the unit
         hp = 100;
+        maxHP = hp;
     }
     
     public bool TakeDamage(int damage)
     {
         hp -= damage;
+
+        float newValue = (2 / maxHP) * hp;
+
+        hpBar.transform.localScale = new Vector3(newValue, 1, 1);
 
         bool isDead = (hp <= 0);
 
